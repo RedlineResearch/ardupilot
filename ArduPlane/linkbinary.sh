1b95add257d5d7a2e85a2e6a4b46f1f66823f0a1
Moses Huang <hhuang05@gmail.com>
2017-02-02 17:03:58 -0500

    Added step to collect the list of files we would like to instrument

--- ArduPlane/linkbinary.sh
+++ ArduPlane/linkbinary.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-AP_HOME="/data/hhuang04/ap3.5.1"
-OSCOPEAPI="/data/hhuang04/autopilotsim/oscope/instrumentation"
-INSTRUMENTER="/data/hhuang04/DroneInstrumenter/build"
+AP_HOME="/home/moses/research/software/ap_plane_bug2835"
+INSTRUMENTER="/home/moses/research/software/codeinstrumenter"
+flist_tmp="filelist_tmp.txt"
+flist="filelist.txt"
 
-# echo "Generating Ardupilot bitcode"
+
+echo "Generating Ardupilot bitcode"
 rm -f $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full.bc
 rm -f $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_instr.bc
 rm -f $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_inject.bc $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_inject.s $AP_HOME/tmp/ArduPlane.build2/ArduPlane.elf
@@ -18,9 +20,22 @@ make instr
 echo "Combine Ardupilot and Instrumentation Bicodes"
 llvm-link -o $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_instr.bc $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full.bc $INSTRUMENTER/oscopeAPI.bc
 
+echo "Build the list of files to instrument"
+# Current directory
+find . -name "*.cpp" | cut -d'/' -f 2- | grep -v "test" >> $flist_tmp
+cd ../libraries
+find `pwd` -name "*.cpp" | grep -v "example" | egrep -v "c\+\+" | grep -v "benchmark" | grep -v "AC_" | grep -v "tests" | grep -v "AP_HAL_FLYMAPLE" | grep -v "AP_HAL_Linux" | grep -v "AP_HAL_PX4" | grep -v "AP_HAL_QURT" | grep -v "AP_HAL_VRBRAIN" | grep -v "utility/Print.cpp" >> ../ArduPlane/filelist_test.txt
+
+cd ../ArduPlane
+echo "# This should contain the filename of the files that need to be instrumented" > $flist
+echo "# To comment out a line,  add # to start of line" >> $flist
+echo "" >> $flist
+sort < $flist_tmp >> $flist
+rm $flist_tmp
+
 echo "Inject instrumentation into Ardupilot bitcode"
 
-$INSTRUMENTER/instrumenter -debug -allF -init_func=_ZN5Plane4loopEv -sfile=$AP_HOME/ArduPlane/filelist.txt -o $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_inject.bc $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_instr.bc 
+$INSTRUMENTER/instrumenter -debug -allF -init_func=_ZN5Plane4loopEv -sfile=$AP_HOME/ArduPlane/$flist -o $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_inject.bc $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_instr.bc 
 
 
 #-arg=/data/hhuang04/ap3.5.1/ArduPlane/varlist.txt
