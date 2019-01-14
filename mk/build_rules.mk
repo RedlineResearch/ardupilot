2ab147389cc9456878cd460384a177860ea9c4f9
Moses Huang <hhuang05@gmail.com>
2019-01-14 16:23:38 -0500

    Removing extra unnecessary commands

--- mk/build_rules.mk
+++ mk/build_rules.mk
@@ -5,19 +5,19 @@
 
 $(BUILDROOT)/%.bc: $(BUILDROOT)/%.cpp $(GENERATE_TARGETS) $(MAVLINK_HEADERS)
 	$(RULEHDR)
-	$(v)$(CXX) $(CXXFLAGS) -c -emit-llvm -Xclang -fdump-record-layouts -o $@ $< $(SKETCH_INCLUDES) > $@.txt
+	$(v)$(CXX) $(CXXFLAGS) -c -emit-llvm -o $@ $< $(SKETCH_INCLUDES) > $@.txt
 
 $(BUILDROOT)/%.bc: $(BUILDROOT)/make.flags $(SRCROOT)/%.cpp $(GENERATE_TARGETS) $(MAVLINK_HEADERS)
 	$(RULEHDR)
-	$(v)$(CXX) $(CXXFLAGS) -c -emit-llvm -Xclang -fdump-record-layouts -o $@ $*.cpp $(SKETCH_INCLUDES) > $@.txt
+	$(v)$(CXX) $(CXXFLAGS) -c -emit-llvm -o $@ $*.cpp $(SKETCH_INCLUDES) > $@.txt
 
 $(BUILDROOT)/%.bc: $(SRCROOT)/%.c
 	$(RULEHDR)
-	$(v)$(CC) $(CFLAGS) -c -emit-llvm -Xclang -fdump-record-layouts -o $@ $< $(SKETCH_INCLUDES) > $@.txt
+	$(v)$(CC) $(CFLAGS) -c -emit-llvm -o $@ $< $(SKETCH_INCLUDES) > $@.txt
 
 $(BUILDROOT)/%.bc: $(SRCROOT)/%.S
 	$(RULEHDR)
-	$(v)$(AS) $(ASFLAGS) -c -emit-llvm -Xclang -fdump-record-layouts -o $@ $< $(SKETCH_INCLUDES) > $@.txt
+	$(v)$(AS) $(ASFLAGS) -c -emit-llvm -o $@ $< $(SKETCH_INCLUDES) > $@.txt
 
 #
 # Build library objects from sources in the sketchbook
@@ -25,12 +25,12 @@ $(BUILDROOT)/%.bc: $(SRCROOT)/%.S
 
 $(BUILDROOT)/libraries/%.bc: $(SKETCHBOOK)/libraries/%.cpp $(GENERATE_TARGETS) $(MAVLINK_HEADERS)
 	$(RULEHDR)
-	$(v)$(CXX) $(CXXFLAGS) -c -emit-llvm -Xclang -fdump-record-layouts -o $@ $< $(SLIB_INCLUDES) > $@.txt
+	$(v)$(CXX) $(CXXFLAGS) -c -emit-llvm -o $@ $< $(SLIB_INCLUDES) > $@.txt
 
 $(BUILDROOT)/libraries/%.bc: $(SKETCHBOOK)/libraries/%.c
 	$(RULEHDR)
-	$(v)$(CC) $(CFLAGS) -c -emit-llvm -Xclang -fdump-record-layouts -o $@ $< $(SLIB_INCLUDES) > $@.txt
+	$(v)$(CC) $(CFLAGS) -c -emit-llvm -o $@ $< $(SLIB_INCLUDES) > $@.txt
 
 $(BUILDROOT)/libraries/%.bc: $(SKETCHBOOK)/libraries/%.S
 	$(RULEHDR)
-	$(v)$(AS) $(ASFLAGS) -c -emit-llvm -Xclang -fdump-record-layouts -o $@ $< $(SLIB_INCLUDES) > $@.txt
+	$(v)$(AS) $(ASFLAGS) -c -emit-llvm -o $@ $< $(SLIB_INCLUDES) > $@.txt
