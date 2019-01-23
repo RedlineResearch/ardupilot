b66ad4a88553b1d46eec1953d149ffb58366ef5e
Moses Huang <hhuang05@gmail.com>
2019-01-23 18:28:41 -0500

    Updated linkbinary to include .h files

--- ArduPlane/linkbinary.sh
+++ ArduPlane/linkbinary.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 COMPILER=g++-5
-LOCAL_APHOME="/home/moses/research/software/ap_plane3.5.1"
+LOCAL_APHOME="/home/moses/research/software/ap_plane_bug7062"
 SERVER_APHOME="/hom/hhuang04/ap_382"
 LOCAL_INSTR="/home/moses/research/software/codeinstrumenter"
 SERVER_INSTR="/home/hhuang04/codeinstr"
@@ -20,7 +20,6 @@ echo $INSTRUMENTER
 flist_tmp="filelist_tmp.txt"
 flist="filelist.txt"
 
-
 echo "Generating Ardupilot bitcode"
 rm -f $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full.bc
 rm -f $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_instr.bc
@@ -37,15 +36,18 @@ else
     make server_instr
 fi
 
-echo "Combine Ardupilot and Instrumentation Bicodes"
+echo "Combine Ardupilot and Instrumentation Bitcodes"
 llvm-link -o $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_instr.bc $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full.bc $INSTRUMENTER/oscopeAPI.bc
 
 echo "Build the list of files to instrument"
 # Current directory
 cd $AP_HOME/ArduPlane
 find . -name "*.cpp" | cut -d'/' -f 2- | grep -v "test" >> $flist_tmp
+find . -name "*.h" | cut -d'/' -f 2- | grep -v "test" >> $flist_tmp
+
 cd $AP_HOME/libraries
 find `pwd` -name "*.cpp" | grep -v "example" | egrep -v "c\+\+" | grep -v "benchmark" | grep -v "AC_" | grep -v "tests" | grep -v "AP_HAL_FLYMAPLE" | grep -v "AP_HAL_Linux" | grep -v "AP_HAL_PX4" | grep -v "AP_HAL_QURT" | grep -v "AP_HAL_VRBRAIN" | grep -v "utility/Print.cpp" >> $AP_HOME/ArduPlane/$flist_tmp
+find `pwd` -name "*.h" | grep -v "example" | egrep -v "c\+\+" | grep -v "benchmark" | grep -v "AC_" | grep -v "tests" | grep -v "AP_HAL_FLYMAPLE" | grep -v "AP_HAL_Linux" | grep -v "AP_HAL_PX4" | grep -v "AP_HAL_QURT" | grep -v "AP_HAL_VRBRAIN" | grep -v "utility/Print.cpp" >> $AP_HOME/ArduPlane/$flist_tmp
 
 cd $AP_HOME/ArduPlane
 echo "# This should contain the filename of the files that need to be instrumented" > $flist
@@ -55,7 +57,8 @@ sort < $flist_tmp >> $flist
 rm $flist_tmp
 
 echo "Inject instrumentation into Ardupilot bitcode"
-$INSTRUMENTER/instrumenter $2 -instrIO -init_func=_ZN5Plane4loopEv -filelist=$AP_HOME/ArduPlane/$flist -o $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_inject.bc $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_instr.bc 
+# $INSTRUMENTER/instrumenter $2 -instrIO -init_func=_ZN5Plane4loopEv -filelist=$AP_HOME/ArduPlane/$flist -o $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_inject.bc $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_instr.bc
+mv $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_instr.bc $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_inject.bc
 
 echo "Compiling to assembly"
 llc -o $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_inject.s $AP_HOME/tmp/ArduPlane.build2/ArduPlane_full_inject.bc 
